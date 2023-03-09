d3386ea274c6d2e14f3c201e73629e25f096cd2e
Tamarabluz <tamarabluz@gmail.com>
2023-03-09 15:04:38 -0300

    logo and search add

---
+++ src/components/Searchbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Searchbar = () =>{
+    return (
+        <div className="searchbar-container">
+            <div className="searchbar">
+                <input placeholder="Buscar Pokemon" />
+            </div>
+        </div>
+    )
+}
+export default Searchbar;
