be7c2dfa03fe0d0aea880d3b756e685cdb34a362
Tamarabluz <tamarabluz@gmail.com>
2023-03-10 12:36:26 -0300

    responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import FavoriteContext from "../contexts/favoritesContext"
 import pokedexlogo from "../assets/pokedexlogo.png"
 import bgclogo from "../assets/bgclogo.png"
@@ -6,23 +6,23 @@ import bgclogo from "../assets/bgclogo.png"
 
 
 const Navbar = () => {
-    const { favoritePokemons } = useContext(FavoriteContext);
-    const logoImg =
-      "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png";
-      
-    return (
-      <nav>
-        <div>
-      <img src={bgclogo} alt="bgc Logo" className="bgc-img" />
-    </div>
-        <div>
-      <img src={pokedexlogo} alt="Pokedex Logo" className="pokedex-img" />
-    </div>
-   
-        <div>Favorites {favoritePokemons.length} ❤️</div>
-      </nav>
-    );
-  };
-  
-  
-  export default Navbar;
+  const { favoritePokemons } = useContext(FavoriteContext);
+  const logoImg =
+    "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png";
+
+  return (
+    <nav>
+      <div>
+        <img src={bgclogo} alt="bgc Logo" className="bgc-img" />
+      </div>
+      <div>
+        <img src={pokedexlogo} alt="Pokedex Logo" className="pokedex-img" />
+      </div>
+
+      <div>Favorites {favoritePokemons.length} ❤️</div>
+    </nav>
+  );
+};
+
+
+export default Navbar;
